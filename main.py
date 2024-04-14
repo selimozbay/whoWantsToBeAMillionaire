fe84e76f00d98e422094b737c6982dabdbd2afec
Selim Özbay <selimozbay@yandex.com>
2024-04-14 13:54:32 +0300

    small question bank added

---
+++ main.py
@@ -0,0 +1,235 @@
+# Who Wants to Be a Millionare
+"""
+("**** ****** **** *** ** **** **** ****?", --> question
+         "*****", --> correct answer
+         "****", --> incorrect answer
+         "****", --> incorrect answer
+         "****") --> incorrect answer
+"""
+
+# all questions
+questions = (
+    # 1st questions
+    (
+        ("Where traditionally do birds fly in winter?",
+         "South",
+         "Ibiza",
+         "East",
+         "North"),
+
+        ("At any one time, what percentage of 5 to 16 year olds in the UK have a mental health problem?",
+         "10%",
+         "1%",
+         "5%",
+         "20%")
+    ),
+    # 2nd questions
+    (
+        ("How many teenagers are believed to self-harm in the UK?",
+         "1 in 15",
+         "1 in 30",
+         "1 in 50",
+         "1 in 100"),
+
+        ("Which of the following is NOT a Bird of Prey?",
+         "Sparrow",
+         "Eagle",
+         "Buzzard",
+         "Owl")
+    ),
+    # 3rd questions
+    (
+        ("Which of these symptoms can happen if you’re depressed?",
+         "Don’t feel hungry",
+         "Hungry all the time",
+         "Always tired",
+         "Any of above"),
+
+        ("Who is the alter ego of Norman Osborn in the 2002 film Spider-Man?",
+         "Green Goblin",
+         "Yellow Peril",
+         "Red Dragon",
+         "Silver Shadow")
+    ),
+    # 4th questions
+    (
+        ("Traditionally, mozzarella cheese is made from the milk of which Animal?",
+         "Buffalo",
+         "Moose",
+         "Goat",
+         "Sheep"),
+
+        ("Which of these are possible triggers for a psychotic episode?",
+         "Taking drugs",
+         "Going to school",
+         "Going shopping",
+         "All of above")
+    ),
+    # 5th questions
+    (
+        ("How many murders are committed in England & Wales in one year by people judged to be mentally ill?",
+         "55",
+         "1555",
+         "555",
+         "5"),
+
+        ("Stratford Saye is the stately home of which Duke?",
+         "Wellington",
+         "Devonshire",
+         "Westminster",
+         "Marlborough")
+    ),
+    # 6th questions
+    (
+        ("It is estimated that since 1985 suicide attempts by young men have...",
+         "Risen by 170%",
+         "Risen by 70%",
+         "Stayed the same",
+         "Fallen by 17%"),
+
+        ("Who wrote the play ‘A Man For All Seasons’?",
+         "Robert Bolt",
+         "Harold Pinter",
+         "Peter Shaffer",
+         "John Osborne")
+    ),
+    # 7th questions
+    (
+        ("Which of the following people has experienced serious mental health problems?",
+         "All of above",
+         "Robbie Williams",
+         "Catherine Tate",
+         "J K Rowling"),
+
+        ("Which breed of dog is traditionally used for hunting hares?",
+         "Beagle",
+         "Dachshund",
+         "Great Dane",
+         "Border Collie")
+    ),
+    # 8th questions
+    (
+        ("How many of the ‘Three Little Pigs’ does the wolf eat?",
+         "Two",
+         "None",
+         "One",
+         "Three"),
+
+        ("Which of the following are considered to be real medical conditions?",
+         "All of above",
+         "High blood pressure",
+         "Anxiety disorders",
+         "Diabetes")
+    ),
+    # 9th questions
+    (
+        ("The Royal Albert Hall was named after the husband of which British monarch?",
+         "Victoria",
+         "Mary",
+         "Mary",
+         "Elizabeth"),
+
+        ("The Black Swan is native to which Country?",
+         "Australia",
+         "Brazil",
+         "Mexico",
+         "India")
+    ),
+    # 10th questions
+    (
+        ("The Starbucks coffee company was founded in which US City?",
+         "Seattle",
+         "Dallas",
+         "Miami",
+         "Boston"),
+
+        ("In which British City was Cary Grant born?",
+         "Bristol",
+         "Birmingham",
+         "Manchester",
+         "Leeds")
+    ),
+    # 11st questions
+    (
+        ("Complete the name of this Cumbrian Town,…-in-Furness?",
+         "Barrow",
+         "Cart",
+         "Dray",
+         "Wagon"),
+
+        ("Which Bank was ‘broken’ by the man in a Victorian music-hall song?",
+         "Monte Carlo",
+         "Cannes",
+         "Lyons",
+         "Nice")
+    ),
+    # 12nd questions
+    (
+        ("According to the title of the film, Brad Pitt’s character spent ‘Seven Years In….’?",
+         "Tibet",
+         "Tanzania",
+         "Texas",
+         "Tipton"),
+
+        ("Who provided the voice of Sinbad in the 2003 film ‘Sinbad: Legend of the Seven Seas’?",
+         "Brad Pitt",
+         "George Clooney",
+         "Edward Norton",
+         "Nicolas Cage"),
+
+        ("Complete the title of the musical by Andrew Lloyd Webber, ‘Tell Me On A …’?",
+         "Sunday",
+         "Monday",
+         "Thursday",
+         "Friday")
+    ),
+    # 13rd questions
+    (
+        ("What was the middle name of the artist LS Lowry?",
+         "Stephen",
+         "Sidney",
+         "Stanley",
+         "Samuel"),
+
+        ("Which Band was formed by Cher’s son?",
+         "Deadsy",
+         "Fatsy",
+         "Livesy",
+         "Patsy"),
+
+        ("Who plays the lawyer Peter Sanderson in the 2003 comedy film ‘Bringing Down The House’?",
+         "Steve Martin",
+         "Chevy Chase",
+         "James Belushi",
+         "Jim Carrey")
+    ),
+    # 14th questions
+    (
+        ("What name is given to the part of a Logarithm after the decimal point?",
+         "Mantissa",
+         "Manumit",
+         "Mantilla",
+         "Manzanilla"),
+
+        ("Which French film is based on the Philippe Djian novel‘37.2 Le Matin’?",
+         "Betty Blue",
+         "Nikita",
+         "Subway",
+         "Breathless")
+    ),
+    # 15th questions
+    (
+        ("What type of play is Shakespeare’s ‘Macbeth’?",
+         "Tragedy",
+         "Musical",
+         "Farce",
+         "Comedy"),
+
+        ("Which of the following is a British bird of prey?",
+         "Red Kite",
+         "Blue Balloon",
+         "Green Train",
+         "Yellow Ba")
+    )
+
+)
